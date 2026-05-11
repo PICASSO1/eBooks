7027d173930ac3e5f14a9c29684c708b68ebc41d
PICASSO1 <neogeo.snk@msa.hinet.net>
2026-05-11 21:58:58 +0800

    Update 1st smaple code (GET_INTERFACE) again.
    - Reorganize the file structure: main.c, `main.h`, get_interface.c, and get_interface.h.
    - To avoid lengthy code, use `goto label` instead (although the teacher DOESN'T recommend this).
    - The `cb_context` structure has been corrected, replacing `valid`, `ack`, and `finish` with `done`.
    - Correct the receiving conditions of `nl_recvmsgs(); `.
    - Re-record Packets and LOGs.

--- NetLink_FrameWork/genl/nl80211/GET_INTERFACE/get_interface.c
+++ NetLink_FrameWork/genl/nl80211/GET_INTERFACE/get_interface.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include "get_interface.h"
 
 int
@@ -11,9 +12,154 @@ void *arg;
     struct cb_context *ctx = (struct cb_context *)arg;
 
     ctx->err = err->error;
-    ctx->ack = 1;
+    ctx->done = 1;
 
     return NL_STOP;
+}
+
+int
+valid_handler(msg, arg)
+struct nl_msg *msg;
+void *arg;
+{   /* In order to avoid the compiler: "warning: unused parameter ‘msg’" when CFLAGs including "-Wall -Wextra"  */
+    (void)arg;
+    struct nlmsghdr *nlh = NULL;
+    struct genlmsghdr *gnlh = NULL;
+    int ret = -1;
+
+    /* 準備一個 attribute 指標表：宣告一個名為 tb 的陣列，而陣列裡的每一個元素都是指到 (struct nlattr *) 的指標 */
+    struct nlattr *tb[NL80211_ATTR_MAX + 1] = { 0 };
+    /* 宣告一個 attribute 的「驗證規則表」，裡面的每一成員都是 struct nla_policy */
+    static struct nla_policy policy[NL80211_ATTR_MAX + 1] = {
+        [NL80211_ATTR_IFINDEX]              = { .type = NLA_U32 },
+        [NL80211_ATTR_IFNAME]               = { .type = NLA_STRING },
+        [NL80211_ATTR_IFTYPE]               = { .type = NLA_U32 },
+        [NL80211_ATTR_WIPHY]                = { .type = NLA_U32 },
+        [NL80211_ATTR_WDEV]                 = { .type = NLA_U64 },
+        [NL80211_ATTR_MAC]                  = { .type = NLA_UNSPEC },
+        [NL80211_ATTR_GENERATION]           = { .type = NLA_U32 },
+        [NL80211_ATTR_4ADDR]                = { .type = NLA_U8  },
+        [NL80211_ATTR_WIPHY_FREQ]           = { .type = NLA_U32 },
+        [NL80211_ATTR_WIPHY_CHANNEL_TYPE]   = { .type = NLA_U32 },
+        [NL80211_ATTR_CHANNEL_WIDTH]        = { .type = NLA_U32 },
+        [NL80211_ATTR_CENTER_FREQ1]         = { .type = NLA_U32 },
+        [NL80211_ATTR_WIPHY_TX_POWER_LEVEL] = { .type = NLA_U32 },
+        [NL80211_ATTR_SSID]                 = { .type = NLA_UNSPEC }
+    };
+
+    /* 取得 Netlink message 的 Header 位址；這是 Netlink Protocol 最外層的表頭 */
+    nlh = nlmsg_hdr(msg);
+    if (!nlh) {
+        fprintf(stderr, "nlmsg_hdr() failed.\n");
+
+        return NL_SKIP;
+    }
+
+    /* 因為 Netlink Protocol 的 Data (Payload)，就是子協定 Generic Netlink 表頭；官方建議直接使用 genlmsg_hdr(); 較為直覺 */
+    gnlh = genlmsg_hdr(nlh);
+/*  gnlh = nlmsg_data(nlh); */
+    if (!gnlh) {
+        fprintf(stderr, "nlmsg_data() failed.\n");
+
+        return NL_SKIP;
+    }
+
+    printf("Generic Family ID: %s (0x%04X) \n", "nl80211", nlh->nlmsg_type);
+    printf("  Length  : %u\n", nlh->nlmsg_len);
+    printf("  Flags   : 0x%04x\n", nlh->nlmsg_flags);
+    printf("    Request          : %X \n", !!((nlh->nlmsg_flags) & NLM_F_REQUEST));
+    printf("    Multipart Message: %X \n", !!((nlh->nlmsg_flags) & NLM_F_MULTI));
+    printf("    Ack:             : %X \n", !!((nlh->nlmsg_flags) & NLM_F_ACK));
+    printf("    Echo:            : %X \n", !!((nlh->nlmsg_flags) & NLM_F_ECHO));
+    printf("    Dump Inconsistent: %X \n", !!((nlh->nlmsg_flags) & 0x0010));
+    printf("    Dump Filtered    : %X \n", !!((nlh->nlmsg_flags) & 0x0020));
+    printf("  Sequence: %u\n", nlh->nlmsg_seq);
+    printf("  Port ID : %u\n", nlh->nlmsg_pid);
+    /* 開始分析 Generic Netlink Header 的資料 */
+    printf("  Command : %s (%u)\n", cmd_to_string(gnlh->cmd), gnlh->cmd);
+    printf("  Version : %u\n", gnlh->version);
+    printf("  Reserved: %u\n", gnlh->reserved);
+
+    /* 已經知道了 Generic Netlink 的資料在哪兒了，開始解譯 attribute */
+    ret = nla_parse(tb, NL80211_ATTR_MAX, genlmsg_attrdata(gnlh, 0), genlmsg_attrlen(gnlh, 0), policy);
+    if (ret < 0) {
+        fprintf(stderr, "nla_parse() failed: %s (%d).\n", nl_geterror(ret), ret);
+
+        return NL_SKIP;
+    }
+
+    if (tb[NL80211_ATTR_IFINDEX])
+        printf("NL80211_ATTR_IFINDEX (%d): %u \n", NL80211_ATTR_IFINDEX, nla_get_u32(tb[NL80211_ATTR_IFINDEX]));
+
+    if (tb[NL80211_ATTR_IFNAME])
+        printf("NL80211_ATTR_IFNAME (%d) : %s \n", NL80211_ATTR_IFNAME, nla_get_string(tb[NL80211_ATTR_IFNAME]));
+
+    if (tb[NL80211_ATTR_WIPHY])
+        printf("NL80211_ATTR_WIPHY (%d): wiphy#%u \n", NL80211_ATTR_WIPHY, nla_get_u32(tb[NL80211_ATTR_WIPHY]));
+
+    if (tb[NL80211_ATTR_IFTYPE]) {
+        enum nl80211_iftype iftype = nla_get_u32(tb[NL80211_ATTR_IFTYPE]);
+        printf("NL80211_ATTR_IFTYPE (%d) : %u (%s) \n", NL80211_ATTR_IFTYPE, iftype, iftype_to_string(iftype));
+    }
+
+    if (tb[NL80211_ATTR_WDEV]) {
+        printf("NL80211_ATTR_WDEV (%d): 0x%016llx (%llu) \n", NL80211_ATTR_WDEV, \
+            (unsigned long long)nla_get_u64(tb[NL80211_ATTR_WDEV]), (unsigned long long)nla_get_u64(tb[NL80211_ATTR_WDEV]));
+    }
+
+    if (tb[NL80211_ATTR_MAC]) {
+        printf("NL80211_ATTR_MAC (%d): ", NL80211_ATTR_MAC );
+        print_mac(nla_data(tb[NL80211_ATTR_MAC]), nla_len(tb[NL80211_ATTR_MAC]));
+        printf("\n");
+    }
+
+    if (tb[NL80211_ATTR_GENERATION]) {
+        uint32_t generation = nla_get_u32(tb[NL80211_ATTR_GENERATION]);
+        printf("NL80211_ATTR_GENERATION (%d) : %u\n", NL80211_ATTR_GENERATION, generation);
+    }
+
+    if (tb[NL80211_ATTR_4ADDR]) {
+        uint8_t use_4addr = nla_get_u8(tb[NL80211_ATTR_4ADDR]);
+        printf("NL80211_ATTR_4ADDR (%d) : %u\n", NL80211_ATTR_4ADDR, use_4addr);
+    }
+
+    if (tb[NL80211_ATTR_WIPHY_FREQ]) {
+        uint32_t freq = nla_get_u32(tb[NL80211_ATTR_WIPHY_FREQ]);
+        printf("NL80211_ATTR_WIPHY_FREQ (%d) : %u MHz\n", NL80211_ATTR_WIPHY_FREQ, freq);
+    }
+
+    if (tb[NL80211_ATTR_WIPHY_CHANNEL_TYPE]) {
+        uint32_t chan_type = nla_get_u32(tb[NL80211_ATTR_WIPHY_CHANNEL_TYPE]);
+        printf("NL80211_ATTR_WIPHY_CHANNEL_TYPE (%d) : %u (%s)\n", NL80211_ATTR_WIPHY_CHANNEL_TYPE, chan_type, channel_type_to_string(chan_type));
+    }
+
+    if (tb[NL80211_ATTR_CHANNEL_WIDTH]) {
+        uint32_t width = nla_get_u32(tb[NL80211_ATTR_CHANNEL_WIDTH]);
+        printf("NL80211_ATTR_CHANNEL_WIDTH (%d) : %u (%s)\n", NL80211_ATTR_CHANNEL_WIDTH, width, channel_width_to_string(width));
+    }
+
+    if (tb[NL80211_ATTR_CENTER_FREQ1]) {
+        uint32_t center1 = nla_get_u32(tb[NL80211_ATTR_CENTER_FREQ1]);
+        printf("NL80211_ATTR_CENTER_FREQ1 (%d) : %u MHz\n", NL80211_ATTR_CENTER_FREQ1, center1);
+    }
+
+    if (tb[NL80211_ATTR_WIPHY_TX_POWER_LEVEL]) {
+        uint32_t tx_power = nla_get_u32(tb[NL80211_ATTR_WIPHY_TX_POWER_LEVEL]);
+        printf("NL80211_ATTR_WIPHY_TX_POWER_LEVEL (%d) : %u (mBm) / %.2f dBm \n", NL80211_ATTR_WIPHY_TX_POWER_LEVEL, tx_power, tx_power / 100.0);
+    }
+
+    if (tb[NL80211_ATTR_SSID]) {
+        int ssid_len = nla_len(tb[NL80211_ATTR_SSID]);
+        unsigned char *ssid = nla_data(tb[NL80211_ATTR_SSID]);
+
+        printf("NL80211_ATTR_SSID (%d) : ", NL80211_ATTR_SSID);
+        print_ssid(ssid, ssid_len);
+        printf("\n");
+    }
+
+    printf("\n");
+
+    return NL_SKIP;
 }
 
 int
@@ -24,7 +170,7 @@ void *arg;
     (void)msg;
     struct cb_context *ctx = (struct cb_context *)arg;
 
-    ctx->finish = 1;
+    ctx->done = 1;
 
     return NL_SKIP;
 }
@@ -37,104 +183,125 @@ void *arg;
     (void)msg;
     struct cb_context *ctx = arg;
 
-    ctx->ack = 1;
+    ctx->done = 1;
 
     return NL_STOP;
 }
 
-const char *
-iftype_to_string(iftype)
-enum nl80211_iftype iftype;
+int 
+dump_interfaces(state)
+struct nl80211_state *state;
 {
-    switch (iftype) {
-        case NL80211_IFTYPE_UNSPECIFIED: return "unspecified";
-        case NL80211_IFTYPE_ADHOC:       return "adhoc";
-        case NL80211_IFTYPE_STATION:     return "station";
-        case NL80211_IFTYPE_AP:          return "ap";
-        case NL80211_IFTYPE_AP_VLAN:     return "ap_vlan";
-        case NL80211_IFTYPE_WDS:         return "wds";
-        case NL80211_IFTYPE_MONITOR:     return "monitor";
-        case NL80211_IFTYPE_MESH_POINT:  return "mesh";
-        case NL80211_IFTYPE_P2P_CLIENT:  return "p2p_client";
-        case NL80211_IFTYPE_P2P_GO:      return "p2p_go";
-        case NL80211_IFTYPE_P2P_DEVICE:  return "p2p_device";
-        case NL80211_IFTYPE_OCB:         return "ocb";
-        case NL80211_IFTYPE_NAN:         return "nan";
-        default:                         return "unknown";
-    }
-}
+    int ret = -1;
+    struct nl_msg *msg = NULL;
+    struct nl_cb *cb = NULL;
+    struct cb_context ctx;
 
-void
-print_mac(mac, len)
-const unsigned char *mac;
-int len;
-{
-    if (!mac || len < 6) {
-        printf("(invalid)");
-        return;
+    bzero(&ctx, sizeof(struct cb_context));
+
+    /* 替 msg 配置一塊記憶體空間 */
+    msg = nlmsg_alloc();
+    if (msg == NULL) {
+        fprintf(stderr, "nlmsg_alloc() failed. \n");
+
+        return -ENOMEM;
     }
 
-    printf("%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-}
+    /* Defined in: /usr/include/libnl3/netlink/handlers.h */
+    cb = nl_cb_alloc(NL_CB_DEFAULT);
+    if (cb == NULL) {
+        fprintf(stderr, "nl_cb_alloc() failed. \n");
+        ret = -ENOMEM;
 
-const char *
-channel_type_to_string(type)
-unsigned int type;
-{
-    switch (type) {
-        case NL80211_CHAN_NO_HT:     return "NL80211_CHAN_NO_HT";
-        case NL80211_CHAN_HT20:      return "NL80211_CHAN_HT20";
-        case NL80211_CHAN_HT40MINUS: return "NL80211_CHAN_HT40MINUS";
-        case NL80211_CHAN_HT40PLUS:  return "NL80211_CHAN_HT40PLUS";
-        default:                     return "UNKNOWN";
+        goto finish;
     }
-}
 
-const char *
-channel_width_to_string(width)
-unsigned int width;
-{
-    switch (width) {
-        case NL80211_CHAN_WIDTH_20_NOHT: return "NL80211_CHAN_WIDTH_20_NOHT";
-        case NL80211_CHAN_WIDTH_20:      return "NL80211_CHAN_WIDTH_20";
-        case NL80211_CHAN_WIDTH_40:      return "NL80211_CHAN_WIDTH_40";
-        case NL80211_CHAN_WIDTH_80:      return "NL80211_CHAN_WIDTH_80";
-        case NL80211_CHAN_WIDTH_80P80:   return "NL80211_CHAN_WIDTH_80P80";
-        case NL80211_CHAN_WIDTH_160:     return "NL80211_CHAN_WIDTH_160";
-        case NL80211_CHAN_WIDTH_5:       return "NL80211_CHAN_WIDTH_5";
-        case NL80211_CHAN_WIDTH_10:      return "NL80211_CHAN_WIDTH_10";
-        default:                         return "UNKNOWN";
+    /* Create Netlink Header + Generic Netlink header
+     * void *
+     * genlmsg_put(msg, port, seq, family, hdrlen, flags, cmd, version)
+     * struct nl_msg *msg;
+     * uint32_t port;         // Port ID
+     * uint32_t seq;          // Sequence
+     * int family;            // Family ID: 0x22 (nl80211)
+     * int hdrlen;
+     * int flags;
+     * uint8_t cmd;           // like: NL80211_CMD_GET_INTERFACE (5)
+     * uint8_t version;       // Family Version
+     */
+    if (!genlmsg_put(msg, NL_AUTO_PORT, NL_AUTO_SEQ, state->nl80211_id, 0, NLM_F_DUMP, NL80211_CMD_GET_INTERFACE, 0)) {
+        fprintf(stderr, "genlmsg_put() failed. \n");
+        ret = -ENOBUFS;
+
+        goto finish;
     }
-}
 
-void
-print_ssid(data, len)
-const unsigned char *data;
-int len;
-{
-    int i = -1;
+    /* 當收到不同種類的 netlink message 時，要呼叫那些對應的 callback function，並傳入甚麼 context */
+    /* int nl_cb_set(cb, type, kind, func, arg)
+     * struct nl_cb *cb;            // Allocate by nl_cb_alloc();
+     * enum nl_cb_type type;        // Defined in: /usr/include/libnl3/netlink/handlers.h
+     * enum nl_cb_kind kind;        // Defined in: /usr/include/libnl3/netlink/handlers.h
+     * nl_recvmsg_msg_cb_t func;    // Callback function
+     * void *arg;                   // Callback function arguments
+     */
+    ret = nl_cb_err(cb, NL_CB_CUSTOM, error_handler, &ctx);
+    if (ret < 0) {
+        fprintf(stderr, "nl_cb_err(error_handler) failed: %s (%d) \n", nl_geterror(ret), ret);
 
-    printf("\"");
-    for (i = 0; i < len; i++) {
-        unsigned char c = data[i];
-
-        if (c >= 32 && c <= 126)
-            printf("%c", c);
-        else
-            printf("\\x%02x", c);
+        goto finish;
     }
-    printf("\"");
-}
 
-const char *
-cmd_to_string(cmd)
-enum nl80211_commands cmd;
-{
-    size_t i = 0, num = sizeof(nl80211_commands_type_map) / sizeof(struct nl80211_commands_type);
+    ret = nl_cb_set(cb, NL_CB_VALID,     NL_CB_CUSTOM, valid_handler, (void *)&ctx);
+    if (ret < 0) {
+        fprintf(stderr, "nl_cb_set(valid_handler) failed: %s (%d) \n", nl_geterror(ret), ret);
 
-    for (i = 0; i < num; i++) {
-        if (nl80211_commands_type_map[i].cmd == cmd)
-            return nl80211_commands_type_map[i].desc;
+        goto finish;
     }
-    return "UNKNOWN";
+
+    ret = nl_cb_set(cb, NL_CB_FINISH,    NL_CB_CUSTOM, finish_handler, (void *)&ctx);
+    if (ret < 0) {
+        fprintf(stderr, "nl_cb_set(finish_handler) failed: %s (%d) \n", nl_geterror(ret), ret);
+
+        goto finish;
+    }
+
+    ret = nl_cb_set(cb, NL_CB_ACK,       NL_CB_CUSTOM,  ack_handler, (void *)&ctx);
+    if (ret < 0) {
+        fprintf(stderr, "nl_cb_set(ack_handler) failed: %s (%d) \n", nl_geterror(ret), ret);
+
+        goto finish;
+    }
+
+    ret = nl_send_auto(state->sock, msg);
+    if (ret < 0) {
+        fprintf(stderr, "nl_send_auto() failed: %s (%d) \n", nl_geterror(ret), ret);
+
+        goto finish;
+    }
+
+    /* 假如還沒有回傳到最後一筆資料，就持續地接收封包；而接收回來的封包會在 NL_CB_VALID 所註冊的 callback 函式處理 */
+    while (ctx.done != 1) {
+        ret = nl_recvmsgs(state->sock, cb);
+        if (ret < 0) {
+            fprintf(stderr, "nl_recvmsgs() failed: %s (%d)\n", nl_geterror(ret), ret);
+            break;
+        }
+    }
+
+    if (ctx.err != 0) {
+        fprintf(stderr, "netlink error: %s (%d)\n", nl_geterror(ctx.err), ctx.err);
+        ret = ctx.err;
+    }
+
+finish:
+    if (cb != NULL) {
+        nl_cb_put(cb);
+        cb = NULL;
+    }
+
+    if (msg != NULL) {
+        nlmsg_free(msg);
+        msg = NULL;
+    }
+
+    return ret;
 }
--- NetLink_FrameWork/genl/nl80211/GET_INTERFACE/get_interface.h
+++ NetLink_FrameWork/genl/nl80211/GET_INTERFACE/get_interface.h
@@ -2,15 +2,7 @@
 #define __GET_INTERFACE_H__
 
 #include <errno.h>
-#include <net/if.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include <linux/nl80211.h>
-
 #include <netlink/attr.h>
 #include <netlink/genl/genl.h>
 #include <netlink/genl/ctrl.h>
@@ -18,41 +10,17 @@
 #include <netlink/netlink.h>
 #include <netlink/socket.h>
 
-struct nl80211_state {
-    struct nl_sock *sock;
-    int nl80211_id;
-};
-
 /* callback context，多個 callback 之間共享「目前收訊流程的狀態」 */
 struct cb_context {
     int err;
-    int valid;
-    int finish;
-    int ack;
-};
-
-struct nl80211_commands_type {
-    enum nl80211_commands cmd;
-    const char *desc;
-};
-
-static const struct nl80211_commands_type nl80211_commands_type_map[] = {
-    /* 不想花時間寫這種東西！沒意義！ */
-    { NL80211_CMD_NEW_INTERFACE, "NL80211_CMD_NEW_INTERFACE" },
+    int done;
 };
 
 int error_handler(struct sockaddr_nl *, struct nlmsgerr *, void *);
-
+int valid_handler(struct nl_msg *, void *);
 int finish_handler(struct nl_msg *, void *);
 int ack_handler(struct nl_msg *, void *);
 
-const char *iftype_to_string(enum nl80211_iftype);
-void print_mac(const unsigned char *, int);
-
-const char *channel_type_to_string(unsigned int);
-const char *channel_width_to_string(unsigned int);
-void print_ssid(const unsigned char *, int);
-
-const char *cmd_to_string(enum nl80211_commands);
+int dump_interfaces(struct nl80211_state *);
 
 #endif    /* __GET_INTERFACE_H__ */
